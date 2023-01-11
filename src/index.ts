a2ccb5684d5994db4963d54292931bc554507506
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-01-11 16:15:58 +0100

    Move message contracts definitions into testapp in order to only publish the Query Editor api (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 export { QueryEditor } from "./QueryEditor";
 export type { QueryEditorProps } from "./QueryEditor";
-export type {
-  MongoQuery,
-  QuerEditorCommand,
-  QueryEditorMessage,
-  QueryResult,
-} from "./messageContract";
