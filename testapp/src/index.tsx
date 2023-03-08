26760d7e0129d34701834b0b31505789302c1edd
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-03-08 11:02:15 +0100

    Update package name. Prepare for npm pack. (#18)

--- testapp/src/index.tsx
+++ testapp/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { QueryEditor, QueryEditorProps, UserQuery } from 'azure-cosmos-db-query-editor';
+import { QueryEditor, QueryEditorProps, UserQuery } from '@azure/cosmos-db-query-editor';
 import { QueryEditorCommand, QueryEditorMessage } from './messageContract';
 import { acquireVsCodeApi } from './vscodeMock';
 
