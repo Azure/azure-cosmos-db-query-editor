2681f00117f849b295f349881490d04537630463
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-10-12 15:51:14 +0200

    Upgrade packages. Remove css errors.

--- jest.config.js
+++ jest.config.js
@@ -136,7 +136,7 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
+    "@testing-library/jest-dom"
   ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
