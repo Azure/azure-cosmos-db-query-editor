2a18d30d6bfef63004beb3322d23b759fd32183b
Laurent Nguyen <laurent.nguyen@microsoft.com>
2024-01-16 15:19:51 +0100

    Try to rename css declaration file

---
+++ src/react-jsondata-editor/lib/declaration.d.ts
@@ -0,0 +1,9 @@
+// declare module "*.css" {
+//   // const content: {[string]: string};
+//   // const content: Record<string, string>;
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const content: any;
+//   export default content;
+// }
+
+declare module "*.css";
